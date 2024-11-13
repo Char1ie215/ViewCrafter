0115eba671cfa854e6c012dfb8771622bf6e5d8a
Haonan Chen <haonan2@illinois.edu>
2024-11-13 16:26:54 -0600

    new single version

--- running.py
+++ running.py
@@ -86,11 +86,11 @@ def run_dust3r(input_images,dust3r_model,clean_pc = True):
         if mode == GlobalAlignerMode.PointCloudOptimizer:
             loss = scene.compute_global_alignment(init='mst', niter=10, schedule='cosine', lr=0.01)
 
-        # if clean_pc:
-        #     self.scene = scene.clean_pointcloud()
-        # else:
-        #     self.scene = scene
-        return scene.clean_pointcloud()  #scene
+        if clean_pc:
+            scene = scene.clean_pointcloud()
+        else:
+            scene = scene
+        return scene  #scene
 
 
 def nvs_sparse_view_interp(images,scene, img_ori, video_length):
